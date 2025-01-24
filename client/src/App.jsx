3f94a68065e62b6aac5cbbf6c140297d04bba02b
princemahmood117 <princemahmood117@gmail.com>
2025-01-24 16:23:29 +0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <h1 className='text-3xl text-blue-500 text-center mt-19'>Tailwind css install done</h1>
+    </>
+  )
+}
+
+export default App
